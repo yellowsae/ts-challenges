4877f480351afc40df293a8e33e1b12a2f68dd3d
yellowsea <yunhai0644@163.com>
2022-05-28 00:30:00 +0800

    chore: add tuple-to-object

--- type-challenges/11-easy-tuple-to-object/template.ts
+++ type-challenges/11-easy-tuple-to-object/template.ts
@@ -23,4 +23,54 @@
 
 /* _____________ 你的代码 _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+// type TupleToObject<T extends readonly any[]> = any
+
+
+
+
+// JS  实现
+
+
+// function tupleToObject(array) {
+
+//   const obj = {}
+
+//   // 2. 循环取值
+//   array.forEach((val) => {
+//     // 赋值为 需要返回的 样子
+//     obj[val] = val
+//   })
+
+//   // 1. 返回一个对象
+//   return obj
+// }
+
+// 目的： 将 array设置为 { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+
+// 步骤： 
+// 1. 返回一个对象
+// 2. 变量 Array
+
+
+
+// const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const  // 测试
+
+
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {  // 1. 返回一个对象
+  // 2. 循环一个TS 中的数组array  [P in T[number]]: P
+  // T[number] 表示下标
+  [P in T[number]]: P   // 赋值  P: P
+}
+  // 主要记住 循环 TS 中的 Array 是 T[number]
+
+
+// type r = TupleToObject<typeof tuple>   // 测试
+
+
+
+/**
+ * 总结： 
+ * 在 TS 中遍历 Array   T[number]
+ */
+
--- type-challenges/11-easy-tuple-to-object/test-cases.ts
+++ type-challenges/11-easy-tuple-to-object/test-cases.ts
@@ -1,4 +1,3 @@
-/* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
@@ -8,7 +7,18 @@ type cases = [
 ]
 
 // @ts-expect-error
+
 type error = TupleToObject<[[1, 2], {}]>
+
+// type r = typeof tuple  // r 的类型： type r = readonly ["tesla", "model 3", "model X", "model Y"]
+
+// as const 
+/**
+ * const 表示常量的意思，
+ * 当 没有写 as const 时 ，  tuple 是一个  数组 string[]
+ * 当 写了 as const 时 ，  tuple 是一个  常量 readonly ["tesla", "model 3", "model X", "model Y"], 数组的值就是 指定的常量
+ */
+
 
 
 
@@ -18,3 +28,28 @@ type error = TupleToObject<[[1, 2], {}]>
   > 查看解答：https://tsch.js.org/11/solutions
   > 更多题目：https://tsch.js.org/zh-CN
 */
+
+
+
+// const  let  在JS中定义变量  （JS世界）
+// type interface   使用在 TypeScript 世界
+
+
+// 1.  将 JS 世界中的语法 ->  TS 世界中的语法  :  使用 typeof  关键字
+// 2. 字面量类型
+
+
+
+
+/**
+ * 2. 字面量类型
+ * 
+ * 字面量类型：表示常量
+let a = "123";
+a = "123aaaa"
+
+type result = typeof a;  // type result = string ;  类型为 string
+
+const strConst = "123"
+type index = typeof strConst;   // type index = "123"  值只能等于 "123",  是一个常量
+ */
